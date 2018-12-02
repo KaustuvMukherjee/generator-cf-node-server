98f0482be12ec45ab950fad3d66b6fd4e2de77bc
Kaustuv Mukherjee <kaustuv.mukherjee@kp.org>
2018-12-02 01:00:20 -0800

    Accomodated changes to avoid any conflict with target jest __test__.

--- test/app.js
+++ test/app.js
@@ -5,16 +5,19 @@ const helpers = require('yeoman-test')
 const rimraf = require('rimraf')
 const CURR_DIR = process.cwd()
 
+const testDir = "test"
+const TIMEOUT = 300000
+
 describe('generator-cf-node-server:app', () => {
     beforeAll(() => {
         return helpers
             .run(path.join(__dirname, '../generators/app'))
             .inDir(path.join(__dirname, './tmp'))
             .withOptions({'skip-install': true})
-    })
+    }, TIMEOUT)
 
     it('Verifying files at /', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}`
         assert.file([`${targetDirectory}/app.js`,
             `${targetDirectory}/index.js`,
             `${targetDirectory}/manifest.yml`,
@@ -22,34 +25,40 @@ describe('generator-cf-node-server:app', () => {
     })
 
     it('Verifying files at /src/controllers', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}/src/controllers`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/controllers`
         assert.file([`${targetDirectory}/healthController.js`])
     })
 
     it('Verifying files at /src/listeners', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}/src/listeners`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/listeners`
         assert.file([`${targetDirectory}/processListener.js`])
     })
 
     it('Verifying files at /src/middlewares', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}/src/middlewares`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/middlewares`
         assert.file([`${targetDirectory}/middleware.js`])
     })
 
+    it('Verifying files at /src/openAPI', () => {
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/openAPI`
+        assert.file([`${targetDirectory}/generateStub.js`])
+        assert.file([`${targetDirectory}/openAPIParser.js`])
+    })
+
     it('Verifying files at /src/routers', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}/src/routers`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/routers`
         assert.file([`${targetDirectory}/router.js`])
     })
 
     it('Verifying files at /src/services', () => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp/${global.appData.userInputs.projectName.val}/src/services`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp/${global.appData.userInputs.projectName.val}/src/services`
         assert.file([`${targetDirectory}/healthService.js`])
     })
 
     afterAll(() => {
-        let targetDirectory = `${CURR_DIR}/__tests__/tmp`
+        let targetDirectory = `${CURR_DIR}/${testDir}/tmp`
         rimraf(targetDirectory, () => {
         })
-    })
+    }, TIMEOUT)
 
 })
